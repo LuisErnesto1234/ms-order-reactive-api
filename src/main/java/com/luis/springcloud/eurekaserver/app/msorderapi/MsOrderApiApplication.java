6461f71effe63fa0dda727ada23637873a39291b
L <145403083+LuisErnesto1234@users.noreply.github.com>
2025-08-20 23:23:41 -0500

    Nuevos cambios V1

---
+++ src/main/java/com/luis/springcloud/eurekaserver/app/msorderapi/MsOrderApiApplication.java
@@ -0,0 +1,13 @@
+package com.luis.springcloud.eurekaserver.app.msorderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsOrderApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsOrderApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/luis/springcloud/eurekaserver/app/msorderapi/MsOrderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luis.springcloud.eurekaserver.app.msorderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsOrderApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
